41ac0bcc90a0e06365798733dc3618d36e72b0b1
shams-shovon <17-34487-2@student.aiub.edu>
2021-08-14 18:37:38 +0600

    delete operation

--- employee_project/employee_register/urls.py
+++ employee_project/employee_register/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.employee_form, name= 'employee_insert'),
     path('<int:id>', views.employee_form, name= 'employee_update'),
+    path('delete/<int:id>/', views.employee_delete, name= 'employee_delete'),
     path('list/', views.employee_list, name= 'employee_list')
 ]
