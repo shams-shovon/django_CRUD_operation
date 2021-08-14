dfc6986a443c47ab58cf4d9fa4d9d590bcec4e98
shams-shovon <17-34487-2@student.aiub.edu>
2021-08-14 11:09:31 +0600

    bootstrap include

--- app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import UserModel
-
-@admin.register(UserModel)
-class UserDataAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'email', 'gender', 'date_created']
